d03cca72311ff1a09bde216478fa9a2c7af7268b
KML M <kamelking2028@gmail.com>
2026-09-25 22:05:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const input = document.getElementById("iptvUrl");
 const btn = document.getElementById("playBtn");
 const video = document.getElementById("player");
 
+/*Lire un URL*/
 btn.addEventListener("click", () => {
   const url = input.value.trim();
   if (!url) return alert("Entre une URL IPTV .m3u8");
 
-  if (Hls.isSupported()) {
+  if (Hls.isSupported()) {     /*vérifie si ton navigateur sait lire les flux HLS*/
     const hls = new Hls();
-    hls.loadSource(url);
-    hls.attachMedia(video);
+    hls.loadSource(url);       /*telecharge la source*/
+    hls.attachMedia(video);    /*relie le flux à la balise <video> dans le HTML.*/
   } else {
     video.src = url;
   }
